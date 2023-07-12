165906143df00ed4489183c7232400b27f210852
Gifty147 <oprahkay5@gmail.com>
2023-07-12 07:03:12 +0300

    first commit

---
+++ average.py
@@ -0,0 +1,5 @@
+# average of five numbers
+numbers = [4, 5, 1, 2, 3, 20]
+average = sum(numbers)/len(numbers)
+print(round(average, 5))
+# average will be sum divided by total list of a
---
+++ variables.py
@@ -0,0 +1,24 @@
+x = 10
+print(x)
+type(x)
+print(type(x))  # printing out the data type of variable x
+y = 10.7
+print(type(y))  # printing out the data (type) of variable y
+x = "name"
+print("name")
+print(type(x))
+# x = 10
+# y = 20
+# z = 30
+(x, y, z) = 10, 20, 30
+print(x, y, z)
+p = [5, 10, 20, 30, 40]
+print(p)
+print(type(p))
+print(p[2])
+print(p[0])
+g = ["dog", "cat", "pig", "tiger", "lion", "goat"]
+print(g)
+print(type(g))
+print(g[2])
+print(g[5])
